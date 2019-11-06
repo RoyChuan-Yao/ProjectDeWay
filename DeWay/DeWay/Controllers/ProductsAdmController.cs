d7531ff00419712966ac31f301e2c7b8b33981c7
aa7894058 <aa7894058@gmail.com>
2019-11-06 11:19:02 +0800

    賣家認證(一部分)管理員首頁機能

---
+++ DeWay/DeWay/Controllers/ProductsAdmController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DeWay.Models;
+
+namespace DeWay.Controllers
+{
+    public class ProductsAdmController : Controller
+    {
+        private shopDBEntities db = new shopDBEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = db.Product.Include(p => p.ProductCategory).Include(p => p.Seller);
+            return View(product.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.ctgID = new SelectList(db.ProductCategory, "pdtCtgID", "fstLayerID");
+            ViewBag.selID = new SelectList(db.Seller, "selID", "selCompany");
+            return View();
+        }
+
+        // POST: Products/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pdtID,selID,pdtName,pdtDate,pdtDescribe,Discontinued,ctgID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ctgID = new SelectList(db.ProductCategory, "pdtCtgID", "fstLayerID", product.ctgID);
+            ViewBag.selID = new SelectList(db.Seller, "selID", "selCompany", product.selID);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ctgID = new SelectList(db.ProductCategory, "pdtCtgID", "fstLayerID", product.ctgID);
+            ViewBag.selID = new SelectList(db.Seller, "selID", "selCompany", product.selID);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pdtID,selID,pdtName,pdtDate,pdtDescribe,Discontinued,ctgID")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ctgID = new SelectList(db.ProductCategory, "pdtCtgID", "fstLayerID", product.ctgID);
+            ViewBag.selID = new SelectList(db.Seller, "selID", "selCompany", product.selID);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Product product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DeWay/DeWay/Controllers/SellerCertificationController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeWay.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DeWay.Controllers
+{
+    public class SellerCertificationController : Controller
+    {
+        shopDBEntities db = new shopDBEntities();
+        // GET: SellerCertification
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IDNumber()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult IDNumber(Seller id)
+        {
+            id.selID = db.Database.SqlQuery<string>("select dbo.GetSellerID()").FirstOrDefault();
+            db.Seller.Add(id);
+            db.SaveChanges();
+            return View(id);
+        }
+
+        public ActionResult GUINumber()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GUINumber(Seller gui)
+        {
+            gui.selID = db.Database.SqlQuery<string>("Select dbo.GetSellerID()").FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                db.Seller.Add(gui);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gui);
+        }
+    }
+}
