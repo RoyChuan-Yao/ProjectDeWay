260f77756cafb78c7af69838b0a1666dd2d3536a
aa7894058 <aa7894058@gmail.com>
2019-11-08 15:28:47 +0800

    sellercertification sp1

--- DeWay/DeWay/Controllers/SellerCertificationController.cs
+++ DeWay/DeWay/Controllers/SellerCertificationController.cs
@@ -6,11 +6,38 @@ using System.Web.Mvc;
 using DeWay.Models;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DeWay.Controllers
 {
     public class SellerCertificationController : Controller
     {
+        SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["shopDBConnectionString"].ConnectionString);
+        SqlCommand Cmd = new SqlCommand();
+        SqlDataAdapter adp = new SqlDataAdapter();
+
+        private void executeSql(string sql)
+        {
+            Cmd.CommandText = sql;
+            Cmd.Connection = Conn;
+
+            Conn.Open();
+            Cmd.ExecuteNonQuery();
+            Conn.Close();
+        }
+
+        private DataTable querySql(string sql)
+        {
+            
+            Cmd.CommandText = sql;
+            Cmd.Connection = Conn;
+            adp.SelectCommand = Cmd;
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+
+            return ds.Tables[0];
+        }
+
         shopDBEntities db = new shopDBEntities();
         // GET: SellerCertification
         public ActionResult Index()
@@ -18,36 +45,80 @@ namespace DeWay.Controllers
             return View();
         }
 
-        public ActionResult IDNumber()
+        public ActionResult IDNumber(string selID)
         {
+            Cmd.Parameters.AddWithValue("@selID", selID);
+            DataRow dt = querySql("select * from Seller where selID=@selID").Rows[0];
+            //string str = "";
+            Seller seller = new Seller();
+            
+            seller.selID = (String)dt["selID"];
+            //seller.selCompany = (String)dt["selCompany"];
+            seller.selCity = (String)dt["selCity"];
+            seller.IDNumber = dt["IDNumber"].ToString();
+            seller.selInfo = (String)dt["selInfo"];
+            seller.selAdress = (String)dt["selAdress"];
+            seller.selDist = (String)dt["selDist"];
 
-            return View();
+            return View(seller);
         }
+
         [HttpPost]
         public ActionResult IDNumber(Seller seller)
         {
-            seller.selID = db.Database.SqlQuery<string>("select dbo.GetSellerID()").FirstOrDefault();
-            db.Seller.Add(seller);
-            db.SaveChanges();
+            string sql = "Update Seller set selCity=@selCity, selDist=@selDist, selAdress=@selAdress, IDNumber=@IDNumber, selInfo=@selInfo" +
+                " where selID=@selID";
+            //Session["memberID"] = seller.mbrID;
+            Cmd.Parameters.AddWithValue("@selID", seller.selID);
+            Cmd.Parameters.AddWithValue("@selCity", seller.selCity);
+            Cmd.Parameters.AddWithValue("@selDist", seller.selDist);
+            Cmd.Parameters.AddWithValue("@selAdress", seller.selAdress);
+            Cmd.Parameters.AddWithValue("@IDNumber", seller.IDNumber);
+            Cmd.Parameters.AddWithValue("@selInfo", seller.selInfo);
+
+            executeSql(sql);
+
             return RedirectToAction("Index");
         }
 
-        public ActionResult GUINumber()
+        public ActionResult GUINumber(string selID)
         {
-            return View();
+            Cmd.Parameters.AddWithValue("@selID", selID);
+            DataRow dt = querySql("select * from Seller where selID=@selID").Rows[0];
+            
+            Seller seller = new Seller();
+
+            seller.selID = (String)dt["selID"];
+            seller.selCompany = dt["selCompany"].ToString();
+            seller.selCity = (String)dt["selCity"];
+            seller.IDNumber = dt["GUINumber"].ToString();
+            seller.selInfo = (String)dt["selInfo"];
+            seller.selAdress = (String)dt["selAdress"];
+            seller.selDist = (String)dt["selDist"];
+
+            return View(seller);
         }
         [HttpPost]
-        public ActionResult GUINumber(Seller gui)
+        public ActionResult GUINumber(Seller seller)
         {
-            gui.selID = db.Database.SqlQuery<string>("Select dbo.GetSellerID()").FirstOrDefault();
-            if (ModelState.IsValid)
-            {
-                db.Seller.Add(gui);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+            string sql = "Update Seller set selCity=@selCity, selDist=@selDist, selCompany=@selCompany," +
+                " selAdress=@selAdress, GUINumber=@GUINumber, selInfo=@selInfo" +
+                " where selID=@selID";
+            //Session["memberID"] = seller.mbrID;
+            Cmd.Parameters.AddWithValue("@selID", seller.selID);
+            Cmd.Parameters.AddWithValue("@selCity", seller.selCity);
+            Cmd.Parameters.AddWithValue("@selDist", seller.selDist);
+            Cmd.Parameters.AddWithValue("@selCompany", seller.selCompany);
+            Cmd.Parameters.AddWithValue("@selAdress", seller.selAdress);
+            Cmd.Parameters.AddWithValue("@GUINumber", seller.GUINumber);
+            Cmd.Parameters.AddWithValue("@selInfo", seller.selInfo);
 
-            return View(gui);
+            executeSql(sql);
+
+            return RedirectToAction("Index");
         }
+
+        
     }
+
 }
