a1eae7c271b70e58720696939381594ea030ba28
RoyChuan-Yao <57085317+RoyChuan-Yao@users.noreply.github.com>
2019-12-25 00:02:41 +0800

    加入 圖片補救

---
+++ DeWay/DeWay/Scripts/CoverMissingProductImages.js
@@ -0,0 +1,8 @@
+﻿var productImageID = [56, 57, 58, 59, 60, 66, 67, 68, 69, 70]
+var imagePath = "/images/img-p-"
+var imageFormat = ".png"
+function GetImage() {
+    var rn = Math.floor(Math.random() * productImageID.length);
+    return imagePath + productImageID[rn] + imageFormat;
+}
+$('img').one("error", (e) => { $(e.target).attr('src', GetImage()) });
